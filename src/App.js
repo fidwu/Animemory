9dad9cfb933a62229e660a8e24c8dc4f7b5ff15c
Susana Marquina <smarq86@gmail.com>
2021-08-28 13:00:41 -0700

    Router and Switch added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MemoryGame from './pages/Game';
+import HomePage from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <MemoryGame />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/game" component={MemoryGame} exact />
+        <Route path="/" component={HomePage} exact />
+      </Switch>
+    </BrowserRouter>
     </div>
   );
 }
