3e665225de0baea4dffd7909dedd4ad2969e8ca1
Susana Marquina <smarq86@gmail.com>
2021-08-28 15:41:09 -0700

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import MemoryGame from './pages/Game';
 import HomePage from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-    <BrowserRouter>
+    <>
+    <Router>
+      <Navbar />
       <Switch>
         <Route path="/game" component={MemoryGame} exact />
         <Route path="/" component={HomePage} exact />
       </Switch>
-    </BrowserRouter>
-    </div>
+    </Router>
+    </>
   );
 }
 
