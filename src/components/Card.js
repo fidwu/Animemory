57ab2ddfad7eb2e00b4b67fd144f4aae236a3232
fidwu <fidellapwu@gmail.com>
2021-08-29 13:37:46 -0700

    display win and lose modal

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,9 +8,13 @@ const Card = (props) => {
 
     const front = () => {
         const image = require(`../assets/${props.img}`).default
-        return props.answer ?
-            <h3>{props.answer && props.name}</h3>
-            : <img src={image} width="70" height="70" alt={`${props.name}`} />
+        return (
+            <div className="card-front">
+                { props.answer ?
+                <h3>{props.answer && props.name}</h3>
+                : <img src={image} width="70" height="70" alt={`${props.name}`} /> }
+            </div>
+        )
     }
 
     return (
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Card from '../components/Card';
 import AnimalData from '../components/AnimalData';
+import ResultModal from "../components/ResultModal";
 
 const Game = () => {
 
@@ -8,6 +9,7 @@ const Game = () => {
     const [compareCards, setCompareCards] = useState([])
     const [matched, setMatched] = useState([])
     const [livesLeft, setLivesLeft] = useState(5)
+    const [result, setResult] = useState("")
 
     useEffect(() => {
         randomize();
@@ -32,10 +34,10 @@ const Game = () => {
     }
 
     const compareCard = (name, answer) => {
-        setCompareCards(prevArray => [...prevArray, name])
         // flip cards when comparing
         setCards(cards.map((card) => {
-            if (card.name === name && card.answer === answer) {
+            if (card.name === name && card.answer === answer && !card.flipped) {
+                setCompareCards(prevArray => [...prevArray, name])
                 card.flipped = true
                 return card
             }
@@ -44,12 +46,10 @@ const Game = () => {
     }
 
     useEffect(() => {
-        console.log(compareCards)
+        // check if the 2 cards match
         if (compareCards.length === 2) {
             const isMatch = compareCards[0] === compareCards[1]
-            console.log(isMatch)
             if (isMatch) {
-                console.log("in here 1")
                 setMatched(prevArray => [...prevArray, compareCards[0]])
             }
             else {
@@ -62,43 +62,51 @@ const Game = () => {
                         return card;
                     }))
                     setLivesLeft(livesLeft => livesLeft - 1)
-                }, 800)
+                }, 600)
             }
             setCompareCards([])
         }
         
         // end game if no strikes left
         if (!livesLeft) {
-            alert("End of game! No more strikes left!")
+            setResult("lose")
         }
-    }, [compareCards, livesLeft])
 
-    const displayStrikes = () => {
-        var strikes = []
-        console.log(livesLeft)
+        // win game if lives left and all cards are matched
+        if (livesLeft && matched.length === AnimalData.length) {
+            setResult("win")
+        }
+    }, [compareCards, livesLeft, matched])
+
+    const displayLives = () => {
+        var heartArr = Array(5)
         // display full heart
         for (let i = 0; i < livesLeft; i++) {
-            console.log("in 1")
-            // strikes = [...strikes, <span className="fas fa-heart" key={i}></span>]
-            strikes = [...strikes, <span className="fa-heart" key={i}>❤</span>]
+            heartArr[i] = <span className="fas fa-heart" key={i}></span>
         }
-        // display outlined heart
-        for (let i = livesLeft; i < 5; i++) {
-            console.log("in 2")
-            strikes = [...strikes, <span className="fa-heart" key={i}>♡</span>]
+        // display heart outline
+        for (let i = heartArr.length - 1; i >= livesLeft; i--) {
+            heartArr[i] = <span className="far fa-heart" key={i}></span>
         }
-        return strikes
+        return heartArr
+    }
+
+    const restartGame = () => {
+        randomize()
+        setCompareCards([])
+        setMatched([])
+        setLivesLeft(5)
+        setResult("")
     }
 
     return (
         <>
             <div className="controls">
-                <div className="strikes">{livesLeft}</div>
-                <div className="strikes-box">
-                    {displayStrikes().map(strikes => strikes)}
+                <div className="lives-left-box">
+                    {displayLives().map(lives => lives)}
                 </div>
-                <div className="timer">01:00</div>
-                <div className="restart">Restart</div>
+                {/* <div className="timer">01:00</div> */}
+                <button className="game-btn restart" onClick={restartGame}>Restart</button>
             </div>
             <div className="grid">
                 {cards.map((animal, index) => (
@@ -112,6 +120,11 @@ const Game = () => {
                     />
                 ))}
             </div>
+            {result &&
+                <ResultModal result={result}>
+                    <button className="game-btn playagain" onClick={restartGame}>Play Again!</button>
+                </ResultModal>
+            }
         </>
     )
 }
