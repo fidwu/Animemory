b21e9bacbaa467b6ce9a55ca051a6e1c469b810e
fidwu <fidellapwu@gmail.com>
2021-08-30 20:08:56 -0700

    instructions section update

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import './Instructions.css';
-import { Button } from './Button';
 
 const Instructions = () => {
   return (
